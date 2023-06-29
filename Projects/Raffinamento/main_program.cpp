da07426c36886648259edf603e3afed9c28ff9ea
Nicolò Briatico <126658354+NicoloBriatico@users.noreply.github.com>
2023-06-29 17:08:20 +0200

    Update main_program.cpp

--- Projects/Raffinamento/main_program.cpp
+++ Projects/Raffinamento/main_program.cpp
@@ -19,48 +19,12 @@ using namespace std;
 // ***************************************************************************
 int main()
 {
-    /*
-     *
-     * ----attenzione a mettere i puntatore e le referenze, perchè il vector fa cambiare il puntatore---
-     *
-     *
-     * Parallelizzare il codice
-     *
-     *
-     * come posso calcolare l'efficienza del mio codicei dal punto di vista di memoria e risorse utilizzate, come faccio a monitorarle
-     * /proc/memInfo solo per linux
-     *
-     *
-     *--- vai sul git di vicini PhDMPI----
-     *
-     *
-     * ---ha senso mantenere l'orientamento dei vertici, se sì come posso fare?---
-     *
-     *TODO
-     * ordinare i vertici in senso antiorario           <- TODO-DA FARE
-     *
-     *
-     * DISCUTI
-     * Fai al posto della matrice di adiacenza una matrice di adiacenza che agli archi asoscia i triangoli adiacenti
-     * Forse conviene mettere gli id al posto degli oggetti per questione di memoria
-     *
-     *TODO
-     *
-     *test tolleranza
-     *
-     *TODO
-     *report con immagini, complessita
-     *TODO
-     *la presentazione slide che prendono le immagine dei report per commentare, non guardare le linee di codice, compplessità computazionale della funzione
-     *non troppe slide
-     */
-
-    //----------------------------------------------------
 
     //imposto una percentuale di triangoli da considerare
     double theta = 0.9;
 
     auto startProgram = chrono::high_resolution_clock::now();
+    
     //inizializzo i vettori che conterranno tutte gli oggetti di classe Vertice, Arco e Triangle
     vector<ShapeLibrary::Vertice> vertici;
     vector<ShapeLibrary::Arco> archi;
@@ -107,22 +71,17 @@ int main()
 
 
     while(numTriPartenza>mesh.triangoli.size()*theta){
-        //auto startRaffinamento= chrono::high_resolution_clock::now();
-
         mesh.RaffinamentoStart();
-
-        //auto endRaffinamento = chrono::high_resolution_clock::now();
-        //chrono::duration<double> elapsedRaffinamento = endRaffinamento - startRaffinamento;
-        //cout<<mesh.triangoli.size()<<"\t"<<elapsedRaffinamento.count()<<endl;
-
-        //cout<<"percentuale di raffinamento: "<<numTriPartenza*100/(mesh.triangoli.size()*theta)<<"%"<<endl;
     }
+    
     auto end1Raffinamento = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed1Raffinamento = end1Raffinamento - start1Raffinamento;
-
+    
     auto startExport= chrono::high_resolution_clock::now();
+    
     //esporto i nuovi dati
     mesh.Esporta();
+    
     auto endExport = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsedExport = endExport - startExport;
 
@@ -134,7 +93,8 @@ int main()
     cout<<"Tempo di Raffinamento: "<<elapsed1Raffinamento.count()<<" sec"<<endl;
     cout<<"Tempo di Raffinamento: "<<elapsedExport.count()<<" sec"<<endl;
     cout<<"Tempo di esecuzione totale: "<<elapsedProgram.count()<<"\t"<<mesh.triangoli.size()<<" sec"<<endl;
-
+    delete mesh;
+    
     return 0;
 
 }
